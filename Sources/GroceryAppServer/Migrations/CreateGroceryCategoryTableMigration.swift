cefbd75f4c1365c8bb1c6254dc1392acfc076a5c
Canned Cat <autthawigorn@gmail.com>
2026-06-03 20:39:39 +0700

    add CreateGroceryCategoryTableMigration

---
+++ Sources/GroceryAppServer/Migrations/CreateGroceryCategoryTableMigration.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  GroceryAppServer
+//
+//  Created by Autthawigorn MBP on 3/6/2569 BE.
+//
+
+import Foundation
+import Fluent
+
+
+struct CreateGroceryCategoryTableMigration: AsyncMigration {
+    
+    func prepare(on database: any Database) async throws {
+        try await database.schema("grocery_categories")
+            .id()
+            .field("title", .string, .required)
+            .field("color_code", .string, .required)
+            .field("user_id", .uuid, .required, .references("users", "id"))
+            .create()
+    }
+    
+    func revert(on database: any Database) async throws {
+        try await database.schema("grocery_categories")
+            .delete()
+    }
+    
+}
