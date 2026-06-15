e64190f61dcd7f7a8d03c99259f093a8f026b165
Canned Cat <autthawigorn@gmail.com>
2026-06-15 22:27:50 +0700

    .

--- Sources/GroceryAppServer/Controllers/GroceryController.swift
+++ Sources/GroceryAppServer/Controllers/GroceryController.swift
@@ -153,7 +153,7 @@ final class GroceryController: RouteCollection, Sendable {
 //            throw Abort(.internalServerError)
 //        }
         
-        // 9. map GroceryItem model to DTO (failable เพราะ groceryItem.id อาจเป็น nil
+        // 9. unwrap groceryItemResponseDTO และ map to DTO for the response
         guard let groceryItemResponseDTO = GroceryItemResponseDTO(groceryItem) else {
             throw Abort(.internalServerError)
         }
